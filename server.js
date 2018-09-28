e5f69f316a7c63ade706d56ea69a56442f2790ea
Sampson Liao <sliao@csumb.edu>
2018-09-27 18:57:15 -0700

    test server.js mod

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./angular-basics'));
+app.use(express.static('./dist/angular-basics'));
 
-app.get('*', function(req,res) {
+app.get('/*', function(req,res) {
 
-  res.sendFile(path.join('./angular-basics/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/angular-basics/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
